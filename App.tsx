1f9fd288bc47092fe908d592ac74b4b49858c23a
youness <benhlalyounes@gmail.com>
2024-07-16 22:56:22 +0200

    create box working with animation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
 // App.tsx
 import * as React from 'react';
-import { ViroARScene, ViroARSceneNavigator, ViroText } from '@reactvision/react-viro';
+import { ViroARScene, ViroARSceneNavigator, ViroMaterials ,ViroBox, ViroAnimations} from '@reactvision/react-viro';
 
 
 
 
 
 const App = () => {
+  
   const  InitialScene= (): JSX.Element =>{
+    ViroMaterials.createMaterials({
+      wood:{
+        diffuseTexture:require('./assets/wood.jpg')
+      }
+    })
+
+    ViroAnimations.registerAnimations({
+      rotate:{
+        duration:2550,
+        properties:{rotateY: '+=90'}
+      }
+    })
+    
+
   return <ViroARScene>
-      <ViroText text={"youness"}
+   {
+/**
+     <ViroText text={"youness"}
         position={[0, 0, -2]}
         style={{fontFamily:'Arial', fontSize:50}}
         >
 
       </ViroText>
+     */  
+   } 
+   <ViroBox
+   height={2}
+   length={2}
+   width={2}
+   scale={[0.2,0.2,0.2]}
+   position={[0,-1,-1]}
+   materials={["wood"]}
+   animation={{name:'rotate',loop:true ,run:true}}
+   ></ViroBox>
     </ViroARScene>;
   }
 
