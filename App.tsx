9694cff9a164d48e1d1695e1c75d07b73e250f6d
youness <benhlalyounes@gmail.com>
2024-07-16 22:38:59 +0200

    basic working

--- App.tsx
+++ App.tsx
@@ -1,85 +1,32 @@
-import {
-  ViroARScene,
-  ViroARSceneNavigator,
-  ViroText,
-  ViroTrackingReason,
-  ViroTrackingStateConstants,
-} from "@reactvision/react-viro";
-import React, { useState } from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+// App.tsx
+import * as React from 'react';
+import { ViroARScene, ViroARSceneNavigator, ViroText } from '@reactvision/react-viro';
 
-const HelloWorldSceneAR = ({ userText }) => {
-  const [text, setText] = useState("Initializing AR...");
 
-  function onInitialized(state: any, reason: ViroTrackingReason) {
-    console.log("onInitialized", state, reason);
-    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText(userText);
-    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-      // Handle loss of tracking
-    }
+
+
+
+const App = () => {
+  const  InitialScene= (): JSX.Element =>{
+  return <ViroARScene>
+      <ViroText text={"youness"}
+        position={[0, 0, -2]}
+        style={{fontFamily:'Arial', fontSize:50}}
+        >
+
+      </ViroText>
+    </ViroARScene>;
   }
 
   return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-    </ViroARScene>
+
+  <ViroARSceneNavigator initialScene={{
+    scene:InitialScene
+  }}
+  style={{flex:1}}>
+
+  </ViroARSceneNavigator>  
   );
 };
 
-const ARApp = () => {
-  const [inputText, setInputText] = useState("");
-  const [submittedText, setSubmittedText] = useState("Hello World!");
-
-  return (
-    <View style={styles.container}>
-      <ViroARSceneNavigator
-        autofocus={true}
-        initialScene={{
-          scene: () => <HelloWorldSceneAR userText={submittedText} />,
-        }}
-        style={styles.f1}
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="Type text to display in AR"
-        value={inputText}
-        onChangeText={(text) => setInputText(text)}
-      />
-      <Button
-        title="Update AR Text"
-        onPress={() => setSubmittedText(inputText)}
-      />
-    </View>
-  );
-};
-
-export default ARApp;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  f1: {
-    flex: 1,
-  },
-  textInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 10,
-  },
-  helloWorldTextStyle: {
-    fontFamily: "Arial",
-    fontSize: 30,
-    color: "#ffffff",
-    textAlignVertical: "center",
-    textAlign: "center",
-  },
-});
+export default App;
