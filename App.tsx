ce58b41bf19f2de644ad2a1a2e2b91b0696d080e
youness <benhlalyounes@gmail.com>
2024-07-17 09:09:37 +0200

    WIP

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 // App.tsx
 import * as React from 'react';
 import { ViroARScene, ViroARSceneNavigator, ViroMaterials ,ViroBox, ViroAnimations} from '@reactvision/react-viro';
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 
 
 
 const App = () => {
-  
+
+  const [object,setObject]= React.useState('lock');
+  const styles =StyleSheet.create({
+    controlView:{
+      width:'100%',
+      height:100,
+      backgroundColor:'#ffffff',
+      display:"flex",
+      flexDirection:"row",
+      justifyContent:'space-between'
+    },
+    mainView: {
+      flex:1
+    }
+
+  })
   const  InitialScene= (): JSX.Element =>{
     ViroMaterials.createMaterials({
       wood:{
@@ -47,14 +64,22 @@ const App = () => {
   }
 
   return (
-
-  <ViroARSceneNavigator initialScene={{
+<View style={styles.mainView}>
+<ViroARSceneNavigator initialScene={{
     scene:InitialScene
   }}
+  viroAppProps={{"object": object}}
   style={{flex:1}}>
 
   </ViroARSceneNavigator>  
-  );
+  <View style={styles.controlView}>
+  <TouchableOpacity onPress={()=>setObject('lock')}><Text>GetLock</Text></TouchableOpacity>
+  <TouchableOpacity onPress={()=>setObject('heart')}><Text>GetHeart</Text></TouchableOpacity>
+
+  </View>
+</View>
+
+   );
 };
 
 export default App;
